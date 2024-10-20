98f3d0cd09e70d954529a6fa47b0ddf5043933bb
UsuarioGithub <correo@github.com>
2024-10-20 11:49:28 -0500

    2

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,18 @@
 import { dataSeries } from './dataSeries.js';
 var datasTbody = document.getElementById('series');
 var totalCreditElm = document.getElementById("total-credits");
-//btnfilterByName.onclick = () => applyFilterByName();
 renderCoursesInTable(dataSeries);
 totalCreditElm.innerHTML = "".concat(getTotalCredits(dataSeries));
 function renderCoursesInTable(series) {
     console.log('Desplegando cursos');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td>").concat(serie.name, "</td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td><a href=\"#\" onclick=\"showDetail(").concat(serie.id, ")\">").concat(serie.name, "</a></td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
         datasTbody.appendChild(trElement);
     });
 }
-/**
-function applyFilterByName() {
-  let text = inputSearchBox.value;
-  text = (text == null) ? '' : text;
-  clearCoursesInTable();
-  let coursesFiltered: Serie[] = searchCourseByName(text, dataSeries);
-  renderCoursesInTable(coursesFiltered);
-}
-
-function searchCourseByName(nameKey: string, series: Serie[]) {
-  return nameKey === '' ? dataSeries : series.filter( c =>
-    c.name.match(nameKey));
-}**/
 function getTotalCredits(series) {
     var promedioSerie = 0;
     series.forEach(function (serie) { return promedioSerie = promedioSerie + serie.seasons; });
     return promedioSerie / series.length;
 }
-/**
-function clearCoursesInTable() {
-  while (datasTbody.hasChildNodes()) {
-    if (datasTbody.firstChild != null) {
-      datasTbody.removeChild(datasTbody.firstChild);
-     
-    }
-  }
-}**/ 
--- scripts/serie.js
+++ scripts/serie.js
@@ -13,11 +13,14 @@ export class Serie {
     }
 }**/
 var Serie = /** @class */ (function () {
-    function Serie(id, name, channel, seasons) {
+    function Serie(id, name, channel, seasons, image, description, url) {
         this.id = id;
         this.name = name;
         this.channel = channel;
         this.seasons = seasons;
+        this.image = image;
+        this.description = description;
+        this.url = url;
     }
     return Serie;
 }());
